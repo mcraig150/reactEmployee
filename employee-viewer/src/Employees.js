bf1db84cc09562e6f91f55cbb6a548168f619771
mcraig150 <mcraig150@gmail.com>
2020-09-07 19:51:11 -0400

    Added filter buttons

--- employee-viewer/src/Employees.js
+++ employee-viewer/src/Employees.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Employees.css";
 import Employee from "./Employee";
+import { Button } from "@material-ui/core";
 
 class Employees extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      role: "managers",
+    };
+  }
+
+  handleClick = (event) => {
+    this.setState({ role: event.target.innerText });
+  };
+
   render() {
     let filter = this.props.workers.filter((worker) => {
       return (
@@ -11,11 +23,40 @@ class Employees extends React.Component {
       );
     });
 
+    let roleFilter = filter.filter((worker) => {
+      return (
+        worker.job
+          .toLowerCase()
+          .indexOf(
+            this.state.role
+              .toLowerCase()
+              .substring(0, this.state.role.length - 1)
+          ) === -1
+      );
+    });
+    console.log(roleFilter);
     return (
       <div className="employees">
-        {filter.map((emp) => (
-          <Employee emp={emp} />
-        ))}
+        <div className="buttons">
+          <Button variant="outlined" onClick={this.handleClick}>
+            All
+          </Button>
+          <Button variant="outlined" onClick={this.handleClick}>
+            Managers
+          </Button>
+          <Button variant="outlined" onClick={this.handleClick}>
+            Engineers
+          </Button>
+          <Button variant="outlined" onClick={this.handleClick}>
+            Interns
+          </Button>
+        </div>
+
+        <div className="employees">
+          {roleFilter.map((emp) => (
+            <Employee emp={emp} />
+          ))}
+        </div>
       </div>
     );
   }
